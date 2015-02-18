637a81f99bcefff7da41421f20dd71c20f7eb245
i053322 <yossi.balan@sap.com>
2015-02-18 09:35:22 +0200

    add native app

---
+++ BabyWatchNative/Babywatch/app/src/main/java/com/example/sap/babywatch/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.sap.babywatch;
+
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    private void handleStart(){
+        showMessage("Start");
+    }
+
+    private void handleStop(){
+        showMessage("Stop");
+    }
+
+    private void showMessage(String str){
+        Context context = getApplicationContext();
+        CharSequence text = str;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+    public void onToggleClicked(View view){
+        // Is the toggle on?
+        boolean isStart = ((ToggleButton) view).isChecked();
+
+        if (isStart) {
+               this.handleStart();
+        } else {
+            this.handleStop();
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
